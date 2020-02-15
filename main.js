d621230d2377a5d9e719ba03439b4e8bcf3953e2
Moises Valdez <moisesvaldez075@gmail.com>
2020-02-15 11:08:17 -0800

    finish all function, in functional programming

--- main.js
+++ main.js
@@ -8,17 +8,35 @@ const user = {
 const compose = (f, g) => (...args) => f(g(...args));
 
 function purchaseItem(...fns) {
-    fns.reduce(compose)
+    return fns.reduce(compose)
 }
 
-console.log(purchaseItem(emptyCart, buyItem, applyTaxToItems, addItemToCart)(user, { name: 'laptop', price: 1300 }));
+console.log(purchaseItem(emptyCart, buyItem, applyTaxToItems, addItemToCart)(user, { name: 'laptop', price: 1500 }));
 
 
 
-function addItemToCart() {}
+function addItemToCart(user, item) {
+    const updateCart = user.cart.concat(item)
+    return Object.assign({}, user, { cart: updateCart });
+}
 
-function applyTaxToItems() {}
+function applyTaxToItems(user) {
+    const { cart } = user;
+    const taxRate = 1.0725;
+    const updateCart = cart.map(item => {
+        return {
+            name: item.name,
+            price: item.price * taxRate
+        }
+    })
 
-function buyItem() {}
+    return Object.assign({}, user, { cart: updateCart })
+}
 
-function emptyCart() {}
+function buyItem(user) {
+    return Object.assign({}, user, { purchases: user.cart });
+}
+
+function emptyCart(user) {
+    return Object.assign({}, user, { cart: [] });
+}
